d209bced45b7fcedadd504dc695d8c0ec4b71a3d
Mirailton F <mirailtonfo@gmail.com>
2023-08-21 10:30:25 -0300

    show image

--- DIP_project/mainwindow.cpp
+++ DIP_project/mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    image.load("../DIP_project/dog.jpg");
+    scene = new QGraphicsScene(this);
+    scene->addPixmap(image);
+    scene->setSceneRect(image.rect());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->imagem->setScene(scene);
 }
 
